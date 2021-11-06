c7a2f3fb24557f9f0eceabdf8dc8c17906ca28f4
Ewa Trzemżalska <trzemson@gmail.com>
2021-11-06 23:17:25 +0100

    Refactoring Functions

--- giggin/src/giggin/components/gigs.cljs
+++ giggin/src/giggin/components/gigs.cljs
@@ -3,17 +3,18 @@
             [giggin.helpers :refer [format-price]]))
 
 (defn gigs []
-  [:main
-   [:div.gigs
-    (for [{:keys [id img title price desc]} (vals @state/gigs)]
-      [:div.gig {:key id}
-       [:img.gig__artwork {:src img :alt title}]
-       [:div.gig__body
-        [:div.gig__title
-         [:div.btn.btn--primary.float--right.tooltip
-          {:data-tooltip "Add to order"
-           :on-click (fn []
-                       (swap! state/orders update id inc))}
-          [:i.icon.icon--plus]] title]
-        [:p.gig__price (format-price price)]
-        [:p.gig_desc desc]]])]])
+  (let [add-to-order (fn [id]
+                       (swap! state/orders update id inc))]
+    [:main
+     [:div.gigs
+      (for [{:keys [id img title price desc]} (vals @state/gigs)]
+        [:div.gig {:key id}
+         [:img.gig__artwork {:src img :alt title}]
+         [:div.gig__body
+          [:div.gig__title
+           [:div.btn.btn--primary.float--right.tooltip
+            {:data-tooltip "Add to order"
+             :on-click #(add-to-order id)}
+            [:i.icon.icon--plus]] title]
+          [:p.gig__price (format-price price)]
+          [:p.gig_desc desc]]])]]))
